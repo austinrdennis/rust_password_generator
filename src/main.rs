3f373fd43c5bac38fc1fa88665d5b5fcb3109eef
Austin Dennis <austinrdennis@gmail.com>
2023-09-25 23:10:31 -0700

    Added user selectable password generation options

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::io;
-use rand::Rng;
+use rand::seq::SliceRandom;
 use clearscreen::ClearScreen;
 
 fn main() {
     let mut length: u8;
+    let mut feature_flags = [false; 3];
     
     length = loop {
         clear_screen();
@@ -14,8 +15,8 @@ fn main() {
             .expect("Failed to read line");
 
         length = match buffer.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+            Ok(number) => number,
+            Err(_)     => continue,
         };
 
         if length < 5 {
@@ -28,27 +29,119 @@ fn main() {
         break length;
     };
 
+    // Use numbers?
+    feature_flags[0] = loop {
+        clear_screen();
+        println!("YES/NO: Should the password contain numbers (0-9)?");
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read line");
 
-    let password: String = generate_password(length).into_iter().collect();
+        let buffer = buffer.to_lowercase();
+        let input = buffer.trim();
+
+        feature_flags[0] = match input {
+            "y"     => true,
+            "yes"   => true,
+            "true"  => true,
+            "n"     => false,
+            "no"    => false,
+            "false" => false,
+            _       => continue,
+        };
+
+        break feature_flags[0];
+    };
+
+    // Use capitol letters?
+    feature_flags[1] = loop {
+        clear_screen();
+        println!("YES/NO: Should the password contain CAPITAL LETTERS?");
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read line");
+
+        let buffer = buffer.to_lowercase();
+        let input = buffer.trim();
+
+        feature_flags[1] = match input {
+            "y"     => true,
+            "yes"   => true,
+            "true"  => true,
+            "n"     => false,
+            "no"    => false,
+            "false" => false,
+            _       => continue,
+        };
+
+        break feature_flags[1]
+    };
+
+    // Use special characters?
+    feature_flags[2] = loop {
+        clear_screen();
+        println!("YES/NO: Should the password contain special characters (@, %, !, etc)?");
+        let mut buffer = String::new();
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read line");
+
+        let buffer = buffer.to_lowercase();
+        let input = buffer.trim();
+
+        feature_flags[2] = match input {
+            "y"     => true,
+            "yes"   => true,
+            "true"  => true,
+            "n"     => false,
+            "no"    => false,
+            "false" => false,
+            _       => continue,
+        };
+
+        break feature_flags[2];
+    };
+
+    let password: String = generate_password(length, feature_flags).into_iter().collect();
 
     clear_screen();
     println!("Length: {length}");
+    println!("Using numbers (0-9): {}", feature_flags[0]);
+    println!("Using CAPTIAL LETTERS: {}", feature_flags[1]);
+    println!("Using special characters (@, %, !, etc): {}", feature_flags[2]);
     println!("Password: {password}");
 }
 
-fn generate_password(length: u8) -> Vec<char> {
-   let mut password: Vec<char> = Vec::new();
+fn generate_password(length: u8, feature_flags: [bool; 3]) -> Vec<char> {
+    let lowercase_range = 'a'..='z';
+    let numbers_range = '0'..='9';
+    let uppercase_range = 'A'..='Z';
+    let special_range = "!@#$%^&*()_+-=[]{}|;':,./<>?".chars();
+    let mut password: Vec<char> = Vec::new();
+    let mut chars_pool:Vec<char> = lowercase_range.collect();
 
-   let mut i = 0;
-   while i < length {
-        let character: u32 = rand::thread_rng().gen_range(48..=122);
-        let character: char = char::from_u32(character).unwrap(); 
+    if feature_flags[0] {
+        chars_pool.extend(numbers_range);  
+    }
+    if feature_flags[1] {
+        chars_pool.extend(uppercase_range);  
+    }
+    if feature_flags[2] {
+        chars_pool.extend(special_range);  
+    }
+
+    let mut i = 0;
+    while i < length {
+        let character: char = *chars_pool.choose(&mut rand::thread_rng()).unwrap();
         password.push(character);
         i += 1;
-   }
+    }
 
-   password
+    password
 }
+
 
 fn clear_screen() {
     ClearScreen::default()
